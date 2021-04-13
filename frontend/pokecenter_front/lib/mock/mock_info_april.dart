4712b5819addcf4c5e3d56073422d288d4f01af5
Agronault <xande.luis@hotmail.com>
2021-04-13 00:19:32 -0300

    feat(info): adds info page generic structure

--- frontend/pokecenter_front/lib/mock/mock_info_april.dart
+++ frontend/pokecenter_front/lib/mock/mock_info_april.dart
@@ -1,8 +1,32 @@
 final List<Map<String, dynamic>> infoReports = [
   {
     "title": "Novo Laboratório Coronel Eduardo Cunha",
-    "srcUrl": "https://exame.com/wp-content/uploads/2020/07/WhatsApp-Image-2020-06-30-at-15.12.56.jpg",
+    "srcUrl": "assets/images/report1_thumb.jpg",
     "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
     "isActive": true
-  }
+  },
+  {
+    "title": "Novo Laboratório Coronel Eduardo Cunha",
+    "srcUrl": "assets/images/report1_thumb.jpg",
+    "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
+    "isActive": true
+  },
+  {
+    "title": "Novo Laboratório Coronel Eduardo Cunha",
+    "srcUrl": "assets/images/report1_thumb.jpg",
+    "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
+    "isActive": true
+  },
+  {
+    "title": "Novo Laboratório Coronel Eduardo Cunha",
+    "srcUrl": "assets/images/report1_thumb.jpg",
+    "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
+    "isActive": true
+  },
+  {
+    "title": "Novo Laboratório Coronel Eduardo Cunha",
+    "srcUrl": "assets/images/report1_thumb.jpg",
+    "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
+    "isActive": true
+  },
 ];
--- frontend/pokecenter_front/lib/pages/tabs/info.dart
+++ frontend/pokecenter_front/lib/pages/tabs/info.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:pokecenter_front/globals.dart';
+import 'package:pokecenter_front/utils/info_report.dart';
+import '../../mock/mock_info_april.dart' as mock;
 
 class InfoTab extends StatelessWidget {
-  const InfoTab({ Key? key }) : super(key: key);
+  const InfoTab({Key? key}) : super(key: key);
+
+  Widget buildReportCard(InfoReport report) {
+    return Container(
+      child: Card(
+          child: ListTile(
+        leading: CircleAvatar(backgroundImage: AssetImage(report.srcUrl)),
+        title: Text(report.title),
+        subtitle: Text(report.text),
+      )),
+    );
+  }
+
+  List<Widget> buildHeading(String title) {
+    return [
+      Divider(),
+      Text(
+        title,
+        style: titleStyle,
+      ),
+      Padding(padding: EdgeInsets.all(10))
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Info',
-            ),
-          ],
-        ),
-      );
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ...buildHeading("Informações:"),
+          ...buildHeading("Novidades:"),
+          ...mock.infoReports
+              .map((report) => buildReportCard(InfoReport.fromJson(report)))
+              .toList()
+        ]),
+      ),
+    );
   }
 }
