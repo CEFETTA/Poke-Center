84a88766590dccb58368763615f96b81383e23b9
Agronault <xande.luis@hotmail.com>
2021-04-13 20:48:43 -0300

    feat(info): sets history steps

--- frontend/pokecenter_front/lib/mock/mock_company_info.dart
+++ frontend/pokecenter_front/lib/mock/mock_company_info.dart
@@ -1,14 +1,14 @@
 final List<Map<String, dynamic>> companyInfos = [
   {
-    "srcUrl": "assets/images/report1_thumb.jpg",
+    "srcUrl": "assets/images/old_build_step.jpg",
     "text": "Desde 1997 A PokeCenter vem tratando pessoas e pokémon todos os dias",
   },
   {
     "srcUrl": "assets/images/report1_thumb.jpg",
-    "text": "Desde 1997 A PokeCenter vem tratando pessoas e pokémon todos os dias",
+    "text": "Em 2005 o primeiro centro de atendimento brasileiro foi inaugurado",
   },
   {
-    "srcUrl": "assets/images/report1_thumb.jpg",
-    "text": "Desde 1997 A PokeCenter vem tratando pessoas e pokémon todos os dias",
+    "srcUrl": "assets/images/vacine_step.jpg",
+    "text": "Em 2015 a PokeCenter ganhou o Prêmio Nobel pela vacina universal para todas as doenças",
   },
 ];
