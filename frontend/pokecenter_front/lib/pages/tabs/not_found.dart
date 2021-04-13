869375c5ee441088c5a7f2d85434c8d1556330c1
Agronault <xande.luis@hotmail.com>
2021-04-12 21:59:10 -0300

    feat(not-found): adjusts image width

--- frontend/pokecenter_front/lib/pages/tabs/not_found.dart
+++ frontend/pokecenter_front/lib/pages/tabs/not_found.dart
@@ -9,7 +9,7 @@ class NotFound extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image(image: AssetImage('assets/images/notfound.png')),
+          Image(image: AssetImage('assets/images/notfound.png'), width: 300,),
           Padding(padding: EdgeInsets.all(20),),
           Text('Página não encontrada, peço perdão pelo vacilo', style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
         ],
