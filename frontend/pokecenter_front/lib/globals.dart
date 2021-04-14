381262de86374aad0cc47073e72a34457bb5f6a2
Thiago Fernandes Dornelles <thiago.fdornelles@gmail.com>
2021-04-13 22:38:25 -0300

    chore: login api post

--- frontend/pokecenter_front/lib/globals.dart
+++ frontend/pokecenter_front/lib/globals.dart
@@ -1,6 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pokecenter_front/stores/counter.dart';
 
 final counter = new Counter();
 final locationUrl = "https://g.page/cefetmg-campus2?share";
 final titleStyle = TextStyle(fontSize: 30);
+
+var options = BaseOptions(
+  baseUrl: 'https://api-pokemon-center.herokuapp.com/',
+  connectTimeout: 20000,
+  receiveTimeout: 20000,
+);
+
+final dio = Dio(options);
