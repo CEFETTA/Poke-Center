cbdf7cc9be2ea983bcfe77a434fa6e12cbd20553
Agronault <xande.luis@hotmail.com>
2021-04-14 11:28:44 -0300

    feat(home): updates reports

--- frontend/pokecenter_front/lib/mock/mock_info_april.dart
+++ frontend/pokecenter_front/lib/mock/mock_info_april.dart
@@ -6,27 +6,21 @@ final List<Map<String, dynamic>> infoReports = [
     "isActive": true
   },
   {
-    "title": "Novo Laboratório Coronel Eduardo Cunha",
-    "srcUrl": "assets/images/report1_thumb.jpg",
-    "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
+    "title": "Novo vírus ataca mais de mil pokémons em Sinnoh",
+    "srcUrl": "assets/images/nt2.jpg",
+    "text": "O corokemon é o novo vírus letal que está se espalhando pelas mais diversas regiões do continente Pokémon.",
     "isActive": true
   },
   {
-    "title": "Novo Laboratório Coronel Eduardo Cunha",
-    "srcUrl": "assets/images/report1_thumb.jpg",
-    "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
+    "title": "Novo PokeCenter",
+    "srcUrl": "assets/images/clinica.jpg",
+    "text": "Nesta manhã (14/04/21) foi inaugurado o mais novo e tecnológico PokeCenter.",
     "isActive": true
   },
   {
-    "title": "Novo Laboratório Coronel Eduardo Cunha",
-    "srcUrl": "assets/images/report1_thumb.jpg",
-    "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
+    "title": "Campanha de vacinação contra o novo vírus.",
+    "srcUrl": "assets/images/pikachu_doente.jpg",
+    "text": "A campanha de vacinação contra o corokemon nesta etapa gerá para pokemons do tipo planta. Essa vacina deixarão os pokémons imunes ao novo vírus.",
     "isActive": true
-  },
-  {
-    "title": "Novo Laboratório Coronel Eduardo Cunha",
-    "srcUrl": "assets/images/report1_thumb.jpg",
-    "text": "O novo laboratório será aberto na próxima segunda, todos virão prestigiar",
-    "isActive": true
-  },
+  }
 ];
